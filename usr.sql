aae171468fd40c816e21fe24fb6f8ec0c9af867e
KamilGos <44849247+KamilGos@users.noreply.github.com>
2019-03-20 21:46:40 +0100

    Delete usr.sql
    Nie potrzebny plik (dane z pliku zawarte w pliku głównym)

--- usr.sql
+++
@@ -1,6 +0,0 @@
-/*alter table pacjenci
-add constraint fk_pacjent_oddzial foreign key (oddzial_nfz)
-references oddzialy_nfz(id_oddzialu);*/
-
-alter table oddzialy_nfz
-add constraint pk_oddzialy primary key (id_oddzialu);
